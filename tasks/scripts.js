91f81238c03ddc13ff8d2bc4cc856f4b7d1f33fe
Josh Davenport <josh.davenport@tghp.co.uk>
2020-08-11 14:32:54 +0100

    Added noop for theme directories with no scripts. E.g. a parent theme

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,22 +1,28 @@
 const scriptEntries = require('../helpers/script-entries');
 const browserifyHelper = require('../helpers/browserify');
 const mergeStream = require('merge-stream');
-
+const noop = require('gulp-noop');
 
 const scripts = () => {
     const themePaths = global.gulppress.getThemePaths();
 
-    return mergeStream(themePaths.map(themePath =>
-        mergeStream(scriptEntries(`${themePath}/assets/src/js`).map(entryScript =>
-            browserifyHelper.gulpify(
-                browserifyHelper.bundle(
-                    browserifyHelper.instance(entryScript)
-                ),
-                entryScript,
-                themePath
-            )
-        ))
-    ));
+    return mergeStream(themePaths.map(themePath => {
+        const scriptEntriesForThemePath = scriptEntries(`${themePath}/assets/src/js`);
+        
+        if (scriptEntriesForThemePath) {
+            return mergeStream(scriptEntriesForThemePath.map(entryScript =>
+                browserifyHelper.gulpify(
+                    browserifyHelper.bundle(
+                        browserifyHelper.instance(entryScript)
+                    ),
+                    entryScript,
+                    themePath
+                )
+            ))
+        } else {
+            return noop();
+        }
+    }));
 };
 scripts.displayName = 'scripts';
 scripts.description = 'Compile JavaScript scripts using browserify';
