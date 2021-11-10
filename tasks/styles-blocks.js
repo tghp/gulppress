85dc0b98fd630254e1aeb886237b906b1cd76e21
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2021-11-10 10:11:03 +0000

    Added block styles task

---
+++ tasks/styles-blocks.js
@@ -0,0 +1,61 @@
+const glob = require('glob');
+const { src, dest } = require('gulp');
+const plumber = require('gulp-plumber');
+const mergeStream = require('merge-stream');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss  = require('gulp-postcss');
+const rename = require('gulp-regex-rename');
+const postCssDefaultPlugins = require('../helpers/postcss-default-plugins');
+const onError = require('../helpers/on-error');
+
+sass.compiler = require('sass');
+
+const stylesBlocks = () => {
+    const themePaths = global.gulppress.getThemePaths();
+    const eventDispatcher = global.gulppress.getEventDispatcher();
+
+    const plugins = eventDispatcher.emitFilter(
+        'postcss.plugins',
+        postCssDefaultPlugins()
+    );
+
+    return mergeStream(themePaths.map(themePath => {
+        // Create stream
+        let stream = src('./assets/src/sass/components/blocks/!(*_abstract*).scss', {sourcemaps:false, cwd: themePath, base: ''})
+            .pipe(plumber({ errorHandler: onError('styles:blocks') }))
+            .pipe(sourcemaps.init())
+            .pipe(rename(/^_/, 'block--'));
+
+        // Allow filters to add to stream at the start
+        stream = eventDispatcher.emitFilter(['task.styles.stream.post-src', 'stream.post-src'], stream, { streamName: 'styles' });
+
+        // Add to stream for this task
+        stream = stream.pipe(sass({
+                includePaths: ['node_modules'],
+                outputStyle: 'compressed',
+            }));
+
+        // Pipe through PostCSS
+        stream = stream.pipe(postcss(plugins));
+
+        // Allow filters to add to stream at the end
+        stream = eventDispatcher.emitFilter(['task.styles.stream.pre-dest', 'stream.pre-dest'], stream, { streamName: 'styles' });
+
+        // Write sourcemaps
+        stream = stream.pipe(sourcemaps.write('.'))
+
+        // Dest stream to output
+        stream = stream.pipe(dest('./assets/dist/css/', {cwd: themePath, base: ''}));
+
+        return eventDispatcher.emitFilter(['task.styles.stream.end', 'stream.end'], stream, { streamName: 'styles' });
+    }));
+};
+stylesBlocks.displayName = 'styles:blocks';
+stylesBlocks.description = 'Compile SCSS stylesheets for blocks';
+
+if(glob.sync('./src/themes/{*,*/*}/assets/src/sass/components/blocks/*').length) {
+  module.exports = stylesBlocks;
+} else {
+  module.exports = null;
+}
